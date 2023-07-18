959e3e28c38227e1719645b59ad5629907a73b92
Carl Masak <carl@masak.org>
2023-07-18 21:21:52 +0800

    Clear out a lot of stuff; rename

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import Nav from './components/Nav';
-import Flower from './components/Flower';
 
 const App = (props) => (
   <Fragment>
     <header className="App-header">
-      <h1 className="App-title">☺springkids☺</h1>
+      <h1 className="App-title">syntax-driven compilation</h1>
     </header>
-    <Nav />
-    <Flower />
   </Fragment>
 );
 
