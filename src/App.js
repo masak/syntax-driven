fd1e8db5d782f1f928c26c224df7b9b2e36eb4b7
Carl Masak <carl@masak.org>
2023-07-18 22:01:58 +0800

    Write some text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ const App = (props) => (
     <header className="App-header">
       <h1 className="App-title">syntax-driven compilation</h1>
     </header>
+
+    <p>
+        a good compiler can be created from a bad compiler,
+        which is better than no compiler.
+        that is the premise of this project, which uses
+        <em>syntax-driven compilation</em> as its basis.
+    </p>
+
+    <p>
+        what does it mean for syntax to drive the
+        compilation? the abstract syntax tree (ast) has
+        different types of nodes (for example, one node for
+        if statements, another node for calling the "car"
+        primitive). each such type of node has a small bit
+        of translation logic. we traverse the tree, run each
+        bit of translation logic based on the nodes that we
+        find, and... that's it, really.
+    </p>
+
   </Fragment>
 );
 
