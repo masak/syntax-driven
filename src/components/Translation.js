c2b7c3ba336ca4e2592edd34b9293b2bc09d9b6d
Carl Masak <carl@masak.org>
2023-07-19 21:59:39 +0800

    Handle de-indentation

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Translation = (props) => (
-  let source = props.source
+function stripSurroundingEmpty(s) {
+  return s.
     .replace(/^( )*\n/, "")
     .replace(/\n( )*$/, "");
+}
 
-  let target = props.target
-    .replace(/^( )*\n/, "")
-    .replace(/\n( )*$/, "");
+function deindent(s) {
+  let lines = s.split(/\n/);
+  let minIndent = 99;
+  for (let line of lines) {
+    let m = s.match(/^( *)/);
+    let n = m[1].length;
+    if (n < minIndent) {
+      minIndent = n;
+    }
+  }
+  return lines.map(
+    (line) => line.substring(minIndent)
+  ).join("\n");
+}
+
+const Translation = (props) => (
+  let source = stripSurroundingEmpty(props.source);
+  source = deindent(source);
+
+  let target = stripSurroundingEmpty(props.target);
+  target = deindent(target);
 
   <div class="translation">
     <pre><code>{source}</code></pre>
