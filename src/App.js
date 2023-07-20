7d5ac96b703535c178c2ca32aaee223bad289fa3
Carl Masak <carl@masak.org>
2023-07-20 23:51:44 +0800

    Add some more content

--- src/App.js
+++ src/App.js
@@ -70,11 +70,56 @@ const App = (props) => (
 53 -- -- --     (end of arguments)         { "\n" }
 60 -- r1 --     (apply %r1 &lt;args&gt;)   { "\n" }
 61 tr r1 --     %tr ← (apply %r1 &lt;args&gt;) { "\n" }</span>
-E0 -- r1 sy     (err symbol) if %r1 != nil        { "\n" }
+E0 -- r1 sy     err symbol if %r1 != nil   { "\n" }
 F0 -- r1 --     return %r1                 { "\n" }
 <span class="less-important">F1 -- r1 r2     return %r1 if %r2 != nil  { "\n" }
 F2 -- -- r2     return nil if %r2 == nil   { "\n" }
 F3 -- -- r2     return t   if %r2 == nil</span></code></pre>
+
+    <p>
+        it's a little bit funny that the compiled bytecode of
+        the `no` function spends five instructions on parameter
+        handling (corresponding to the `(x)` parameter list in
+        the Bel code), and then just two instructions on the
+        body. the function has a disproportionately big head.
+    </p>
+
+    <p>
+        right now we're kind of wasting registers; using 4 when
+        we could use only 2. a bytecode function could use up to
+        256 different registers, but of course there is a lot of
+        sense in using as few as we can. we'll come back to this
+        issue a bit later, as we will soon have bigger fish to
+        fry.
+    </p>
+
+    <p>
+        let's look at the translation of the next function,
+        <code>atom</code>.
+    </p>
+
+    <Translation
+      source={`
+        (def atom (x)
+          (no (id (type x) 'pair)))
+      `}
+
+      target={`
+        %0 ← params
+        err 'underargs if %0 == nil
+        %1 ← (cdr %0)
+        err 'overargs if %1 != nil
+        %2 ← (car %0)
+        %3 ← (get-global "no")
+        %4 ← (type %1)
+        %5 ← (id %4 pair)
+        (arg-start)
+        (arg-one %5)
+        (arg-end)
+        %6 ← (apply %3)
+        return %6
+      `}
+    />
   </main>
 );
 
