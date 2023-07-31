0be229a6e0aa7b141b6f80d2508907017f33dafe
Carl Masak <carl@masak.org>
2023-07-31 23:24:53 +0800

    Parse the entire target, not just instructions

--- compiler-src/parse-target.ts
+++ compiler-src/parse-target.ts
@@ -8,9 +8,12 @@ import {
     InstrSetApply,
     InstrSetGetGlobal,
     InstrReturnReg,
+    Target,
 } from "./target";
 
 const EMPTY_LINE = /^\s*$/;
+const HEADER = /^bcfn\s+(\w+)\s*\[req:\s*%(\d+);\s*reg:\s*%0..%(\d+)\]$/;
+
 const SET_PRIM_ID_REG_SYM = /^%(\d+)\s*←\s*\(id\s+%(\d+)\s+'?(\w+)\)$/;
 const SET_PRIM_TYPE_REG = /^%(\d+)\s*←\s*\(type\s+%(\d+)\)$/;
 const ARGS_START = /^\(args-start\)$/;
@@ -20,8 +23,12 @@ const SET_APPLY = /^%(\d+)\s*←\s*\(apply\s+%(\d+)\)$/;
 const SET_GET_GLOBAL = /^%(\d+)\s*←\s*\(get-global\s+"([^"]*)"\)$/;
 const RETURN_REG = /^return\s*%(\d+)$/;
 
-export function parse(input: string): Array<Instr> {
+export function parse(input: string): Target {
     let instrs: Array<Instr> = [];
+
+    let name = "<unset>";
+    let maxReqReg = 0;
+    let maxReg = 0;
 
     for (let line of input.split("\n")) {
         if (EMPTY_LINE.test(line)) {
@@ -31,7 +38,12 @@ export function parse(input: string): Array<Instr> {
         line = line.trim();
 
         let m: RegExpExecArray;
-        if (m = SET_PRIM_ID_REG_SYM.exec(line)!) {
+        if (m = HEADER.exec(line)!) {
+            name = m[1];
+            maxReqReg = Number(m[2]);
+            maxReg = Number(m[3]);
+        }
+        else if (m = SET_PRIM_ID_REG_SYM.exec(line)!) {
             let targetReg = Number(m[1]);
             let leftReg = Number(m[2]);
             let rightSym = m[3];
@@ -73,6 +85,18 @@ export function parse(input: string): Array<Instr> {
         }
     }
 
-    return instrs;
+    if (name === "<unset>") {
+        throw new Error("Parse error: no header line");
+    }
+
+    let req = maxReqReg === 0
+        ? "%0"
+        : `%0..%${maxReqReg}`;
+
+    let reg = maxReg === 0
+        ? "%0"
+        : `%0..%${maxReg}`;
+
+    return new Target(name, { req, reg }, instrs);
 }
 
--- test-data/expected-targets.ts
+++ test-data/expected-targets.ts
@@ -1,29 +1,19 @@
 import {
-    Header,
     Target,
 } from "../compiler-src/target";
 import {
     parse,
 } from "../compiler-src/parse-target";
 
-function target(name: string, header: Header, body: string): Target {
-    return new Target(name, header, parse(body));
-}
-
 let expectedTargets = new Map<string, Target>([
-    ["no", target(
-        "no",
-        { req: "%0", reg: "%0..%1" },
-        `
+    ["no", parse(`
+        bcfn no [req: %0; reg: %0..%1]
             %1 ← (id %0 nil)
             return %1
-        `,
-    )],
+    `)],
 
-    ["atom", target(
-        "atom",
-        { req: "%0", reg: "%0..%4" },
-        `
+    ["atom", parse(`
+        bcfn atom [req: %0; reg: %0..%4]
             %1 ← (get-global "no")
             %2 ← (type %0)
             %3 ← (id %2 'pair)
@@ -32,8 +22,7 @@ let expectedTargets = new Map<string, Target>([
             (args-end)
             %4 ← (apply %1)
             return %4
-        `,
-    )],
+    `)],
 ]);
 
 export default expectedTargets;
