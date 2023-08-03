75e345da24594e756c118ed8ee9c8111acd5d76f
Carl Mäsak <carl@masak.org>
2023-08-03 13:40:44 +0800

    Implement custom pattern-matching

--- compiler-src/parse-target.ts
+++ compiler-src/parse-target.ts
@@ -1,4 +1,5 @@
 import {
+    BaseInstr,
     Instr,
     InstrSetPrimIdRegSym,
     InstrSetPrimTypeReg,
@@ -13,6 +14,83 @@ import {
 
 const EMPTY_LINE = /^\s*$/;
 const HEADER = /^bcfn\s+(\w+)\s*\[req:\s*%(\d+);\s*reg:\s*%0..%(\d+)\]$/;
+
+let parsers: Map<typeof BaseInstr, string> = new Map([
+    [InstrSetPrimIdRegSym, ",reg ← (id ,reg ,sym)"],
+    [InstrSetPrimTypeReg, ",reg ← (type ,reg)"],
+    [InstrArgsStart, "(args-start)"],
+    [InstrArgOne, "(arg-one ,reg)"],
+    [InstrArgsEnd, "(args-end)"],
+    [InstrSetApply, ",reg ← (apply ,reg)"],
+    [InstrSetGetGlobal, ",reg ← (get-global ,str)"],
+    [InstrReturnReg, "return ,reg"],
+] as Array<[typeof BaseInstr, string]>);
+
+const REG = /^%(\d+)/;
+const SYM = /^'?(\w+)/;
+const STR = /^"([^"]*)"/;
+
+function parseInstr(input: string): Instr | null {
+    PATTERN:
+    for (let [constructor, pattern] of parsers.entries()) {
+        let inputPos = 0;
+        let patternPos = 0;
+        let constructorArgs: Array<number | string> = [];
+        let m: RegExpExecArray;
+
+        while (patternPos < pattern.length) {
+            if (pattern.charAt(patternPos) === ",") {
+                patternPos += 1;
+                let commaType = pattern.substring(patternPos, patternPos + 3);
+                if (commaType === "reg") {
+                    if (m = REG.exec(input.substring(inputPos))!) {
+                        inputPos += m[0].length;
+                        constructorArgs.push(Number(m[1]));
+                        patternPos += 3;
+                    }
+                    else {
+                        continue PATTERN;
+                    }
+                }
+                else if (commaType === "sym") {
+                    if (m = SYM.exec(input.substring(inputPos))!) {
+                        inputPos += m[0].length;
+                        constructorArgs.push(m[1]);
+                        patternPos += 3;
+                    }
+                    else {
+                        continue PATTERN;
+                    }
+                }
+                else if (commaType === "str") {
+                    if (m = STR.exec(input.substring(inputPos))!) {
+                        inputPos += m[0].length;
+                        constructorArgs.push(m[1]);
+                        patternPos += 3;
+                    }
+                    else {
+                        continue PATTERN;
+                    }
+                }
+                else {
+                    throw new Error(`Unknown comma type: ${commaType}`);
+                }
+            }
+            else {
+                if (inputPos >= input.length) {
+                    continue PATTERN;
+                }
+                if (input.charAt(inputPos) !== pattern.charAt(patternPos)) {
+                    continue PATTERN;
+                }
+                inputPos += 1;
+                patternPos += 1;
+            }
+        }
+        return new constructor(...constructorArgs);
+    }
+    return null;
+}
 
 export function parse(input: string): Target {
     let instrs: Array<Instr> = [];
@@ -37,35 +115,12 @@ export function parse(input: string): Target {
             maxReg = Number(m[3]);
             continue;
         }
-        else if (InstrSetPrimIdRegSym.matches(line)) {
-            instr = InstrSetPrimIdRegSym.parse(line);
-        }
-        else if (InstrSetPrimTypeReg.matches(line)) {
-            instr = InstrSetPrimTypeReg.parse(line);
-        }
-        else if (InstrArgsStart.matches(line)) {
-            instr = InstrArgsStart.parse(line);
-        }
-        else if (InstrArgOne.matches(line)) {
-            instr = InstrArgOne.parse(line);
-        }
-        else if (InstrArgsEnd.matches(line)) {
-            instr = InstrArgsEnd.parse(line);
-        }
-        else if (InstrSetApply.matches(line)) {
-            instr = InstrSetApply.parse(line);
-        }
-        else if (InstrReturnReg.matches(line)) {
-            instr = InstrReturnReg.parse(line);
-        }
-        else if (InstrSetGetGlobal.matches(line)) {
-            instr = InstrSetGetGlobal.parse(line);
+        else if (instr = parseInstr(line)!) {
+            instrs.push(instr);
         }
         else {
             throw new Error(`Unrecognized line: '${line}'`);
         }
-
-        instrs.push(instr);
     }
 
     if (name === "<unset>") {
--- compiler-src/target.ts
+++ compiler-src/target.ts
@@ -13,155 +13,60 @@ export type Instr =
     InstrSetGetGlobal |
     InstrReturnReg;
 
-export class InstrSetPrimIdRegSym {
+export class BaseInstr {
+    constructor(...args: any) {
+    }
+}
+
+export class InstrSetPrimIdRegSym extends BaseInstr {
     constructor(
         public targetReg: number,
         public leftReg: number,
         public rightSym: string,
     ) {
-    }
-
-    static re =
-        /^%(\d+)\s*←\s*\(id\s+%(\d+)\s+'?(\w+)\)$/;
-
-    static matches(input: string) {
-        return InstrSetPrimIdRegSym.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrSetPrimIdRegSym.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrSetPrimIdRegSym(Number(m[1]), Number(m[2]), m[3]);
+        super();
     }
 }
 
-export class InstrSetPrimTypeReg {
+export class InstrSetPrimTypeReg extends BaseInstr {
     constructor(public targetReg: number, public objectReg: number) {
-    }
-
-    static re =
-        /^%(\d+)\s*←\s*\(type\s+%(\d+)\)$/;
-
-    static matches(input: string) {
-        return InstrSetPrimTypeReg.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrSetPrimTypeReg.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrSetPrimTypeReg(Number(m[1]), Number(m[2]));
+        super();
     }
 }
 
-export class InstrArgsStart {
-    static re = /^\(args-start\)$/;
-
-    static matches(input: string) {
-        return InstrArgsStart.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrArgsStart.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrArgsStart();
+export class InstrArgsStart extends BaseInstr {
+    constructor() {
+        super();
     }
 }
 
-export class InstrArgOne {
+export class InstrArgOne extends BaseInstr {
     constructor(public register: number) {
-    }
-
-    static re = /^\(arg-one\s*%(\d+)\)$/;
-
-    static matches(input: string) {
-        return InstrArgOne.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrArgOne.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrArgOne(Number(m[1]));
+        super();
     }
 }
 
-export class InstrArgsEnd {
-    static re = /^\(args-end\)$/;
-
-    static matches(input: string) {
-        return InstrArgsEnd.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrArgsEnd.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrArgsEnd();
+export class InstrArgsEnd extends BaseInstr {
+    constructor() {
+        super();
     }
 }
 
-export class InstrSetApply {
+export class InstrSetApply extends BaseInstr {
     constructor(public targetReg: number, public funcReg: number) {
-    }
-
-    static re = /^%(\d+)\s*←\s*\(apply\s+%(\d+)\)$/;
-
-    static matches(input: string) {
-        return InstrSetApply.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrSetApply.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrSetApply(Number(m[1]), Number(m[2]));
+        super();
     }
 }
 
-export class InstrSetGetGlobal {
+export class InstrSetGetGlobal extends BaseInstr {
     constructor(public targetReg: number, public name: string) {
-    }
-
-    static re = /^%(\d+)\s*←\s*\(get-global\s+"([^"]*)"\)$/;
-
-    static matches(input: string) {
-        return InstrSetGetGlobal.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrSetGetGlobal.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrSetGetGlobal(Number(m[1]), m[2]);
+        super();
     }
 }
 
-export class InstrReturnReg {
+export class InstrReturnReg extends BaseInstr {
     constructor(public returnReg: number) {
-    }
-
-    static re = /^return\s*%(\d+)$/;
-
-    static matches(input: string) {
-        return InstrReturnReg.re.test(input);
-    }
-
-    static parse(input: string) {
-        let m = InstrReturnReg.re.exec(input);
-        if (m === null) {
-            throw new Error(`Couldn't parse '${input}'`);
-        }
-        return new InstrReturnReg(Number(m[1]));
+        super();
     }
 }
 
