cc76f4336301c1e4643b4cb0225dbb8fc5066b4a
Carl Masak <carl@masak.org>
2023-08-30 22:09:30 +0800

    First stab at optimized 'all'

--- test-data/targets-opt.ts
+++ test-data/targets-opt.ts
@@ -19,6 +19,36 @@ let expectedTargets = new Map<string, Target>([
             %3 ← (id %2 nil)
             return %3
     `)],
+
+    ["all", parse(`
+        bcfn all [req: %0..%1; reg: %0..%10]
+            %2 ← (id %1 nil)
+            jmp :if-branch-1 unless %2
+            %3 ← (get-symbol "t")
+            %10 ← %3
+            jmp :if-end-1
+          :if-branch-1
+            %4 ← (car %1)
+            (args-start)
+              (arg-one %4)
+            (args-end)
+            %5 ← (apply %0)
+            jmp :if-branch-2 unless %5
+            %6 ← (cdr %1)
+            %7 ← (get-global "all")
+            (args-start)
+              (arg-one %0)
+              (arg-one %6)
+            (args-end)
+            %8 ← (apply %7)
+            %10 ← %8
+            jmp :if-end-1
+          :if-branch-2
+            %9 ← (get-symbol "nil")
+            %10 ← %9
+          :if-end-1
+            return %10
+    `)],
 ]);
 
 export default expectedTargets;
--- test/02-compile.ts
+++ test/02-compile.ts
@@ -7,16 +7,18 @@ import {
     stringifyTarget,
 } from "../src/target";
 import {
+    OPT_ALL,
     OPT_NONE,
 } from "../src/conf";
 
 import sources from "../test-data/sources";
+import targetsOpt from "../test-data/targets-opt";
 import targetsUnopt from "../test-data/targets-unopt";
 import {
     envAfterAtom,
 } from "../test-data/envs";
 
-test("compiling 'all'", (t) => {
+test("compiling 'all' (unopt)", (t) => {
     let source = sources.get("all")!;
     t.not(source, undefined);
 
@@ -26,6 +28,20 @@ test("compiling 'all'", (t) => {
     let expectedTarget = stringifyTarget(target);
     let actualTarget =
         stringifyTarget(compile(source, envAfterAtom, OPT_NONE));
+
+    t.deepEqual(actualTarget, expectedTarget);
+});
+
+test("compiling 'all' (opt)", (t) => {
+    let source = sources.get("all")!;
+    t.not(source, undefined);
+
+    let target = targetsOpt.get("all")!;
+    t.not(target, undefined);
+
+    let expectedTarget = stringifyTarget(target);
+    let actualTarget =
+        stringifyTarget(compile(source, envAfterAtom, OPT_ALL));
 
     t.deepEqual(actualTarget, expectedTarget);
 });
