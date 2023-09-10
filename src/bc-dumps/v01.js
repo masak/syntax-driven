8d4b92ae238ca3ec5867800111f1dc5f4e044b46
Carl Masak <carl@masak.org>
2023-09-10 22:21:07 +0800

    Add v01 bytecode dump

---
+++ src/bc-dumps/v01.js
@@ -0,0 +1,155 @@
+export const bcDump = {
+    "entries": [
+        {
+            "type": "header",
+            "bytes": [
+                0, 48, 0, 56
+            ]
+        },
+        {
+            "type": "intern",
+            "addr": 4,
+            "size": 4,
+            "category": 1,
+            "categoryName": "symbol",
+            "ref": 24,
+            "bytes": [
+                1, 0, 0, 24
+            ]
+        },
+        {
+            "type": "intern",
+            "addr": 8,
+            "size": 4,
+            "category": 1,
+            "categoryName": "symbol",
+            "ref": 28,
+            "bytes": [
+                1, 0, 0, 28
+            ]
+        },
+        {
+            "type": "intern",
+            "addr": 12,
+            "size": 4,
+            "category": 1,
+            "categoryName": "symbol",
+            "ref": 32,
+            "bytes": [
+                1, 0, 0, 32
+            ]
+        },
+        {
+            "type": "intern",
+            "addr": 16,
+            "size": 4,
+            "category": 1,
+            "categoryName": "symbol",
+            "ref": 40,
+            "bytes": [
+                1, 0, 0, 40
+            ]
+        },
+        {
+            "type": "intern",
+            "addr": 20,
+            "size": 4,
+            "category": 1,
+            "categoryName": "symbol",
+            "ref": 28,
+            "bytes": [
+                1, 0, 0, 28
+            ]
+        },
+        {
+            "type": "string",
+            "addr": 24,
+            "size": 4,
+            "content": "no",
+            "bytes": [
+                2, 110, 111, 0
+            ]
+        },
+        {
+            "type": "string",
+            "addr": 28,
+            "size": 4,
+            "content": "nil",
+            "bytes": [
+                3, 110, 105, 108
+            ]
+        },
+        {
+            "type": "string",
+            "addr": 32,
+            "size": 8,
+            "content": "atom",
+            "bytes": [
+                4, 97, 116, 111,
+                109, 0, 0, 0
+            ]
+        },
+        {
+            "type": "string",
+            "addr": 40,
+            "size": 8,
+            "content": "pair",
+            "bytes": [
+                4, 112, 97, 105,
+                114, 0, 0, 0
+            ]
+        },
+        {
+            "type": "global",
+            "addr": 48,
+            "size": 4,
+            "name": "no",
+            "ref": 56,
+            "bytes": [
+                0, 4, 0, 56
+            ]
+        },
+        {
+            "type": "global",
+            "addr": 52,
+            "size": 4,
+            "name": "atom",
+            "ref": 68,
+            "bytes": [
+                0, 12, 0, 68
+            ]
+        },
+        {
+            "type": "bcfn",
+            "addr": 56,
+            "size": 12,
+            "name": "no",
+            "req": 1,
+            "reg": 2,
+            "instrCount": 2,
+            "bytes": [
+                1, 0, 2, 2,
+                1, 1, 0, 0,
+                50, 0, 1, 0
+            ]
+        },
+        {
+            "type": "bcfn",
+            "addr": 68,
+            "size": 20,
+            "name": "atom",
+            "req": 1,
+            "reg": 4,
+            "instrCount": 4,
+            "bytes": [
+                1, 0, 4, 4,
+                3, 1, 0, 0,
+                0, 2, 1, 16,
+                1, 3, 2, 0,
+                50, 0, 3, 0
+            ]
+        }
+    ],
+    "totalByteDumpSize": 88
+};
+
