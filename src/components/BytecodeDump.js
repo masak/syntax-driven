7f4c3a7d5f4a542f954d5f975185dc4b8735ce85
Carl Masak <carl@masak.org>
2023-09-11 22:33:59 +0800

    Improve the colors in the bytecode dump

--- src/components/BytecodeDump.js
+++ src/components/BytecodeDump.js
@@ -3,40 +3,55 @@ import React from 'react';
 const xOffset = 10;
 const yOffset = 10;
 const width = 680;
-const height = 50;
+const height = 28;
 const padding = 4;
 const bytesPerRow = 64;
 const byteWidth = (width - 4 - 4) / bytesPerRow;
 const byteHeight = 6;
-const rowDistance = 20;
+const rowDistance = 12;
 
-const headerColorScheme = [
-    "#f60", "#f60", "#f70", "#f70",
-    "#f80", "#f90", "#f90", "#fa0",
-    "#fb0", "#fb0", "#fc0", "#fd0",
-    "#fd0", "#fe0", "#fe0", "#ff0",
-];
+function clamp(v) {
+    return v < 0
+        ? 0
+        : v > 255
+            ? 255
+            : v;
+}
 
-const internColorScheme = [
-    "#888", "#888", "#999", "#999",
-    "#aaa", "#aaa", "#bbb", "#bbb",
-    "#ccc", "#ccc", "#ddd", "#ddd",
-    "#eee", "#eee", "#fff", "#fff",
-];
+function interpolate(lowRgb, highRgb, byte) {
+    let val = byte / 255;
+    let r = lowRgb[0] * (1 - val) + highRgb[0] * val;
+    let g = lowRgb[1] * (1 - val) + highRgb[1] * val;
+    let b = lowRgb[2] * (1 - val) + highRgb[2] * val;
+    let rValue = clamp(Math.floor(256 * r));
+    let gValue = clamp(Math.floor(256 * g));
+    let bValue = clamp(Math.floor(256 * b));
+    return `rgb(${rValue}, ${gValue}, ${bValue})`;
+}
 
-const stringColorScheme = [
-    "#f60", "#e61", "#d72", "#c73",
-    "#b84", "#a95", "#996", "#8a7",
-    "#7b8", "#6b9", "#5ca", "#4db",
-    "#3dc", "#2ed", "#1fe", "#0ff",
-];
+function headerColor(byte) {
+    return interpolate([1.00, .25, .25], [1.00, 1.00, .75], byte);
+}
 
-const bcfnColorScheme = [
-    "#0ff", "#1ff", "#2ff", "#3ff",
-    "#4ee", "#5ee", "#6ed", "#7dd",
-    "#8dc", "#9cc", "#acb", "#bcb",
-    "#cba", "#dba", "#eba", "#fba",
-];
+function internColor(byte) {
+    return interpolate([1.00, .75, .25], [.75, 1.00, .75], byte);
+}
+
+function stringColor(byte) {
+    return interpolate([.25, .75, .25], [.50, .75, 1.00], byte);
+}
+
+function globalColor(byte) {
+    return interpolate([.25, .50, .75], [.75, .75, 1.00], byte);
+}
+
+function bcfnHeaderColor(byte) {
+    return interpolate([.25, .25, 1.00], [1.00, .75, 1.00], byte);
+}
+
+function bcfnColor(byte) {
+    return interpolate([.75, .25, .75], [1.00, .75, .75], byte);
+}
 
 const BytecodeDump = (props) => {
     let boxes = [];
@@ -44,26 +59,31 @@ const BytecodeDump = (props) => {
     let i = 0;
     for (let entry of entries) {
         let bytes = entry.bytes;
+        let byteOffset = 0;
         for (let byte of bytes) {
             let x = xOffset + padding + (i % bytesPerRow) * byteWidth;
             let y = yOffset + padding +
                 rowDistance * Math.floor(i / bytesPerRow);
-            let colorIndex = Math.floor(byte / 16);
             let color = "#000";
             if (entry.type === "header") {
-                color = headerColorScheme[colorIndex];
+                color = headerColor(byte);
             }
             else if (entry.type === "intern") {
-                color = internColorScheme[colorIndex];
+                color = internColor(byte);
             }
             else if (entry.type === "string") {
-                color = stringColorScheme[colorIndex];
+                color = stringColor(byte);
             }
             else if (entry.type === "global") {
-                color = internColorScheme[colorIndex];
+                color = globalColor(byte);
             }
             else if (entry.type === "bcfn") {
-                color = bcfnColorScheme[colorIndex];
+                if (byteOffset < 4) {
+                    color = bcfnHeaderColor(byte);
+                }
+                else {
+                    color = bcfnColor(byte);
+                }
             }
             boxes.push(
                 <rect x={x}
@@ -75,6 +95,7 @@ const BytecodeDump = (props) => {
                 />
             );
             i += 1;
+            byteOffset += 1;
         }
     }
 
